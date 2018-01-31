1a113871a8fdb071cafd1d792dcd7263871666cb
zdy48195 <zdy48195@ly.com>
2018-01-31 11:17:43 +0800

    类加载器

--- src/main/java/com/java/classloader/ClassLoaderTest.java
+++ src/main/java/com/java/classloader/ClassLoaderTest.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Method;
 public class ClassLoaderTest {
 
 	public static void main(String[] args) throws Exception {
-		DiskClassLoader diskClassLoader=new DiskClassLoader("E:\\\\Study\\\\java\\\\classloader\\\\lib");
+		DiskClassLoader diskClassLoader=new DiskClassLoader("E:\\Study\\java\\classloader\\lib");
 		Class<?> loadClass = diskClassLoader.loadClass("com.classloader.Test");
 		if(loadClass!=null){
 			Object instance = loadClass.newInstance();
