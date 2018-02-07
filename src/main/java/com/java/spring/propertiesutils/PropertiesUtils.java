6c6d1da21ff51df4c468bd51afee6eaf4a61b83c
zdy48195 <zdy48195@ly.com>
2018-02-07 11:34:38 +0800

    properties的加载

---
+++ src/main/java/com/java/spring/propertiesutils/PropertiesUtils.java
@@ -0,0 +1,26 @@
+package com.java.spring.propertiesutils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+public class PropertiesUtils {
+	
+	/**
+	 * 加载类路径下的properties文件
+	 * @param propertiesName
+	 * @return
+	 * @throws IOException
+	 */
+	public static Properties loadClassPathProperties(String propertiesName) throws IOException{
+		ClassLoader classLoader = PropertiesUtils.class.getClassLoader();
+		Properties properties = PropertiesLoaderUtils.loadAllProperties(propertiesName, classLoader);
+		return properties;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Properties properties = loadClassPathProperties("test.properties");
+		System.out.println(properties);
+	}
+}
