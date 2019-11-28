cb5638229bd9587c90ca89a63c6191b12a6746ad
zdy48195 <zuodaoyong@4321>
2019-11-28 10:43:28 +0800

    Channel操作

--- src/main/java/com/netty/nio/FileChannelDemo.java
+++ src/main/java/com/netty/nio/FileChannelDemo.java
@@ -1,5 +1,6 @@
 package com.netty.nio;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channel;
@@ -9,6 +10,37 @@ import java.nio.file.*;
 public class FileChannelDemo {
 
     public static void main(String[] args) throws Exception{
+        //writeFile();
+        //readFile();
+        copyFile();
+    }
+
+    private static void copyFile() throws Exception{
+        FileInputStream inputStream=new FileInputStream("D:\\temp\\hadoop-2.7.2.zip");
+        FileChannel inChannel = inputStream.getChannel();
+        FileOutputStream outputStream=new FileOutputStream("D:\\temp\\netty\\hadoop-2.7.2.zip");
+        FileChannel outChannel = outputStream.getChannel();
+        ByteBuffer byteBuffer=ByteBuffer.allocate(1024);
+        while (true){
+            byteBuffer.clear();
+            int read = inChannel.read(byteBuffer);
+            if(read==-1){
+                break;
+            }
+            byteBuffer.flip();
+            outChannel.write(byteBuffer);
+        }
+    }
+
+    private static void readFile() throws Exception{
+        FileInputStream inputStream=new FileInputStream("D:\\temp\\netty\\1.txt");
+        FileChannel channel = inputStream.getChannel();
+        ByteBuffer byteBuffer=ByteBuffer.allocate(1024);
+        channel.read(byteBuffer);
+        System.out.println(new String(byteBuffer.array()));
+    }
+
+    private static void writeFile() throws Exception{
         String message="hello world";
         FileOutputStream out=new FileOutputStream("D:\\temp\\netty\\t.txt");
         FileChannel fileChannel= out.getChannel();
@@ -18,4 +50,5 @@ public class FileChannelDemo {
         fileChannel.write(byteBuffer);
         out.close();
     }
+
 }
--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -3,16 +3,16 @@ package com.test;
 
 import com.java.algorithm.common.CommonUtils;
 import com.java.functions.SortFunction;
+import com.java.thread.pool.CustomThreadPool;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import static java.lang.Thread.sleep;
 
 public class Test {
 
 	public static void main(String[] args) throws Exception {
-		Thread t1=new Thread();
-		t1.start();
 
 
 	}
