192d5a40b93de128f4459f07b3755c29744ddc24
neo <1525714537@qq.com>
2020-02-17 20:45:57 +0800

    leetcode

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -5,13 +5,16 @@ import java.util.Arrays;
 
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
+        String aa="sdvdf";
+        int d = aa.indexOf('d');
+        System.out.println(d);
+        String aa1 = aa.substring(d+1);
+        System.out.println(aa1);
 
-        int i=10;
-        int j=i/10%10;
-
-        System.out.println(j);
     }
 
 }
+
+    
