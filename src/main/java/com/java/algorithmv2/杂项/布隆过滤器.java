30c4154d5d3135523ea80d7544e19ed5522d8f15
neo <1525714537@qq.com>
2020-02-23 17:59:09 +0800

    引入guava，使用布隆过滤器

--- src/main/java/com/java/algorithmv2/杂项/布隆过滤器.java
+++ src/main/java/com/java/algorithmv2/杂项/布隆过滤器.java
@@ -11,11 +11,12 @@ import java.util.List;
 public class 布隆过滤器 {
     public static void main(String[] args) {
         List<String> list= Arrays.asList(new String[]{"java","python","scala","hadoop"});
+        //fpp:误判率
         BloomFilter bloomFilter=BloomFilter.create(Funnels.stringFunnel(Charsets.UTF_8),list.size(),0.01);
         for(String str:list){
             bloomFilter.put(str);
         }
-        //定义误判率
+        //定义误判数量
         int errorCount=0;
         for(int i=0;i<100;i++){
            String str="abc"+i;
