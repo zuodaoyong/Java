705646ded439c31be26c9b652fcf9444217d182e
neo <1525714537@qq.com>
2020-06-08 07:46:48 +0800

    leetcode

---
+++ src/main/java/com/java/leetcode/t524通过删除字母匹配到字典里最长单词.java
@@ -0,0 +1,60 @@
+package com.java.leetcode;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给定一个字符串和一个字符串字典，找到字典里面最长的字符串，该字符串可以通过删除给定字符串的某些字符来得到。如果答案不止一个，返回长度最长且字典顺序最小的字符串。如果答案不存在，则返回空字符串。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * s = "abpcplea", d = ["ale","apple","monkey","plea"]
+ *
+ * 输出:
+ * "apple"
+ * 示例 2:
+ *
+ * 输入:
+ * s = "abpcplea", d = ["a","b","c"]
+ *
+ * 输出:
+ * "a"
+ */
+public class t524通过删除字母匹配到字典里最长单词 {
+    public static void main(String[] args) {
+
+
+       String s = "abpcplea";
+       List<String> d = Arrays.asList(new String[]{"ale","apple","monkey","plea"});
+        String longestWord = findLongestWord(s, d);
+        System.out.println(longestWord);
+
+    }
+
+    public static String findLongestWord(String s, List<String> d) {
+        String longestWord = "";
+        for (String target : d) {
+            int l1 = longestWord.length(), l2 = target.length();
+            if (l1 > l2 || (l1 == l2 && longestWord.compareTo(target) < 0)) {
+                continue;
+            }
+            if (isSubstr(s, target)) {
+                longestWord = target;
+            }
+        }
+        return longestWord;
+    }
+
+    private static boolean isSubstr(String s, String target) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < target.length()) {
+            if (s.charAt(i) == target.charAt(j)) {
+                j++;
+            }
+            i++;
+        }
+        return j == target.length();
+    }
+}
