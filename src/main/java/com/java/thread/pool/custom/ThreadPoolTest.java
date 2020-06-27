7f585b620d715ebd80a0b692a4c22147befb7700
neo <1525714537@qq.com>
2020-06-27 19:51:20 +0800

    线程池

--- src/main/java/com/java/thread/pool/custom/ThreadPoolTest.java
+++ src/main/java/com/java/thread/pool/custom/ThreadPoolTest.java
@@ -7,7 +7,15 @@ public class ThreadPoolTest {
     public static void main(String[] args) {
 
         ThreadPool threadPool=new ThreadPool(10,2,1000, TimeUnit.MILLISECONDS,(queue,task)->{
+            //1、死等
             queue.put(task);
+            //2、超时等待
+            //queue.offer(task,100,TimeUnit.MILLISECONDS);
+            //3、不处理
+            //4、抛异常
+            //throw new RuntimeException("阻塞队列满");
+            //5、调用者执行
+            //task.run();
         });
 
         for(int i=0;i<5;i++){
