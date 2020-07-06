02d99f23e35e8eff8d83940f2f92a01aed54977e
zdy48195 <zuodaoyong@4321>
2020-07-06 17:04:58 +0800

    leetcode

---
+++ src/main/java/com/java/leetcode/动态规划/t70爬楼梯.java
@@ -0,0 +1,48 @@
+package com.java.leetcode.动态规划;
+
+
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
+ *
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ */
+public class t70爬楼梯 {
+    public static void main(String[] args) {
+
+        int i = climbStairs(6);
+        System.out.println(i);
+    }
+
+    public static int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int p2=1;
+        int p1=2;
+        int curr=0;
+        for(int i=2;i<n;i++){
+            curr=p2+p1;
+            p2=p1;
+            p1=curr;
+        }
+        return curr;
+    }
+}
